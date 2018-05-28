908edd659229e1d0c5f5dbc29757f7e93ac22845
Derick Anderson <anderson.derick.w@gmail.com>
2018-05-28 18:40:58 +0900

    Allow whitespace at the beginning of commands.

--- plugins/listener.py
+++ plugins/listener.py
@@ -18,48 +18,48 @@ from .decorator import not_allowed_in_busy_time, write_log
 
 
 # タイムスタンプの勤怠系
-@listen_to('^おはー$')
-@respond_to('^おはー$')
+@listen_to('^\s*おはー$')
+@respond_to('^\s*おはー$')
 @write_log
 def record_punch_in_listener(message):
     record_punch_in(message)
 
 
-@listen_to(r'^(店じまい|おつー)$')
-@respond_to(r'^(店じまい|おつー)$')
+@listen_to(r'^\s*(店じまい|おつー)$')
+@respond_to(r'^\s*(店じまい|おつー)$')
 @write_log
 def record_punch_out_listener(message, _):
     record_punch_out(message)
 
 
-@respond_to(r'^(?!.*(休憩開始|休憩終了)).*(?=休憩|一時退社|もぐもぐタイム|抜け).+$')
+@respond_to(r'^\s*(?!.*(休憩開始|休憩終了)).*(?=休憩|一時退社|もぐもぐタイム|抜け).+$')
 def help_rest_listener(message, _):
     help_rest(message)
 
 
-@respond_to('^休憩開始$')
+@respond_to('^\s*休憩開始$')
 def record_starting_rest_listener(message):
     record_starting_rest(message)
 
 
-@respond_to('^休憩終了$')
+@respond_to('^\s*休憩終了$')
 def record_ending_rest_listener(message):
     record_ending_rest(message)
 
 
 # 有給系
-@respond_to(r'^(?!.*(に有給取得したいです|残休暇日数)).*(?=有給|休暇).+$')
+@respond_to(r'^\s*(?!.*(に有給取得したいです|残休暇日数)).*(?=有給|休暇).+$')
 def help_holiday_listener(message, _):
     help_holiday(message)
 
 
-@respond_to('^残休暇日数$')
+@respond_to('^\s*残休暇日数$')
 @not_allowed_in_busy_time
 def get_remained_holiday_count_listener(message):
     get_remained_holiday_count(message)
 
 
-@respond_to(r'^\d+年\d+月\d+日に有給取得したいです$')
+@respond_to(r'^\s*\d+年\d+月\d+日に有給取得したいです$')
 @not_allowed_in_busy_time
 def apply_holiday_listener(message):
     apply_holiday(message)
