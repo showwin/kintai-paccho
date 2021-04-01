51b0da22e7543a6b9bb2755d90e5395dcc3e234a
showwin <showwin_kmc@yahoo.co.jp>
2021-04-01 13:28:24 +0900

    [add] エイプリルフールネタリアクション

--- plugins/components/time_recorder.py
+++ plugins/components/time_recorder.py
@@ -1,5 +1,6 @@
 import datetime
 import json
+import random
 
 from ..requester import KOTException, KOTRequester
 from .helper import get_employee_key
@@ -12,7 +13,17 @@ def record_punch_in(message):
 
 def record_punch_out(message):
     if _timerecord(message, '退勤'):
-        message.send(':gas_paccho_1: < おつー　打刻したよー')
+        message_list = [
+            ':gas_paccho_1: < おつー　でも打刻してあげないよ〜:zany_face:',
+            ':gas_paccho_1: < え〜っと、それも嘘なんでしょ:den_paccho2:',
+            ':gas_paccho_1: < 今日はもう疲れたから打刻しないよー:gaspaccho_fall:',
+            ':gas_paccho_1: < おつー　打刻したよー　そういえば今日エイプリルフールらしいね:denpaccho_upper_left::denpaccho_upper_right:',
+            ':gas_paccho_1: < おつー　打刻しすぎて骨折したよー:oni_paccho:',
+            ':gas_paccho_1: < おつー　実は今日結婚したんだ:denpaccho_kakusei:',
+            ':gas_paccho_1: < おつー　有給減らしといたよ〜:magic_paccho:',
+        ]
+        message_index = random.randint(0, 4)
+        message.send(message_list[message_index])
 
 
 def record_starting_rest(message):
