0cf17aaa36603166233569b4b9801e727e847603
Yuuki Takahashi <20282867+yktakaha4@users.noreply.github.com>
2023-06-05 12:54:20 +0900

    add https proxy

--- components/requester.py
+++ components/requester.py
@@ -11,6 +11,7 @@ class KOTException(Exception):
 class KOTRequester:
     KOT_API_BASE_URL = "https://api.kingtime.jp/v1.0"
     KOT_TOKEN = os.environ.get("KOT_TOKEN")
+    KOT_HTTPS_PROXY = os.environ.get("KOT_HTTPS_PROXY")
 
     def __init__(self):
         self.base_url = self.KOT_API_BASE_URL
@@ -18,10 +19,13 @@ class KOTRequester:
             "Content-Type": "application/json; charset=utf-8",
             "Authorization": "Bearer {token}".format(token=self.KOT_TOKEN),
         }
+        self.proxies = {
+            "https": self.KOT_HTTPS_PROXY,
+        } if self.KOT_HTTPS_PROXY else None
 
     def get(self, uri):
         url = self.base_url + uri
-        resp = requests.get(url, headers=self.headers)
+        resp = requests.get(url, headers=self.headers, proxies=self.proxies)
         resp_json = json.loads(resp.text)
         if "errors" in resp_json:
             raise KOTException(resp_json["errors"][0]["message"])
@@ -29,7 +33,7 @@ class KOTRequester:
 
     def post(self, uri, payload):
         url = self.base_url + uri
-        resp = requests.post(url, headers=self.headers, data=payload)
+        resp = requests.post(url, headers=self.headers, data=payload, proxies=self.proxies)
         resp_json = json.loads(resp.text)
         if "errors" in resp_json:
             raise KOTException(resp_json["errors"][0]["message"])
@@ -37,7 +41,7 @@ class KOTRequester:
 
     def put(self, uri, payload):
         url = self.base_url + uri
-        resp = requests.put(url, headers=self.headers, json=payload)
+        resp = requests.put(url, headers=self.headers, json=payload, proxies=self.proxies)
         resp_json = json.loads(resp.text)
         if "errors" in resp_json:
             raise KOTException(resp_json["errors"][0]["message"])
